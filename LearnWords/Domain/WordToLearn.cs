99db42de6a8f1f92104bc12b9fc86aaa6c3cff4c
Vasyl Kosovan <vasyl.kosovan@innroad.com>
2025-01-26 08:39:04 +0200

    initial commit

---
+++ LearnWords/Domain/WordToLearn.cs
@@ -0,0 +1,43 @@
+﻿namespace Domain;
+
+public record Word(string Value,
+                   string Translation,
+                   string ExampleSentence,
+                   Language WordLanguage,
+                   Language TranslationLanguage,
+                   FamiliarityLevel FamiliarityLevel = FamiliarityLevel.Unknown);
+
+public enum Language
+{
+    English,
+    Ukrainian,
+    Spanish
+}
+
+public enum FamiliarityLevel
+{
+    Unknown = 0,
+    Beginner = 1,
+    Intermediate = 2,
+    Advanced = 3,
+    Mastered = 4
+}
+
+public class Account
+{
+    private readonly List<Word> _words = new();
+    public IReadOnlyCollection<Word> Words => _words.AsReadOnly();
+
+    public void AddWord(Word word)
+    {
+        if (!_words.Contains(word))
+        {
+            _words.Add(word);
+        }
+    }
+
+    public bool DeleteWord(Word word)
+    {
+        return _words.Remove(word);
+    }
+}
